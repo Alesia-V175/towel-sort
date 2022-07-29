d6c0fda4b87bfcf7f6cdc677df731a492e87bdd4
Alesia-V175 <vaivasilisa@gmail.com>
2022-07-29 12:40:51 +0300

    feat: impliment towel-sort.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+
+    if (matrix === undefined) {
+        return [];
+    }
+
+    let result = [];
+    for (let i = 0; i < matrix.length; i++) {
+        if (i % 2 === 0) {
+            result[i] = matrix[i];
+        } else if (i % 2 !== 0) {
+            result[i] = matrix[i].reverse();
+        }
+    }
+
+    let singleArr = [];
+    for (let i = 0; i < result.length; i++) {
+        singleArr = singleArr.concat(result[i]);
+    }
+    return singleArr;
 }
